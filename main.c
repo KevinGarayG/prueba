1646adea9f454ee39a6e1ef82b98d864c958d28b
Kevin Garay <kevin.garay2004@gmail.com>
2022-07-20 22:32:27 +0200

    se creo el hola mundo en c

---
+++ main.c
@@ -0,0 +1,14 @@
+/* Programa: Hola mundo (Versión 2) */
+
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+    printf( "\n   Hola mundo." );
+    printf( "\n\n   Pulse una tecla para salir..." );
+
+    getch(); /* Pausa */
+
+    return 0;
+}
